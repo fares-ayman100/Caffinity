cab2eaa267b9edc1875b039b47d38f4320bb29aa
Fares Ayman <fareshe73@gmail.com>
2026-02-16 02:14:44 +0200

    edit in db connection

--- server.js
+++ server.js
@@ -1,34 +1,32 @@
-const dotenv = require('dotenv').config();
+const dotenv = require('dotenv');
+dotenv.config();
+
 const app = require('./app');
-const mongoose = require('mongoose');
 
 process.on('uncaughtException', (err) => {
   console.log(err.name, err.message);
   process.exit(1);
 });
 
-DB = process.env.DATABASE;
-mongoose.connect(DB).then(() => {
-  console.log('Connection Successful');
-});
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`🚀Server running on port ${port}`);
+
+const server = app.listen(port, () => {
+  console.log(`🚀 Server running on port ${port}`);
 });
 
-//Unhandled Rejection
+/* Unhandled Rejection */
 process.on('unhandledRejection', (err) => {
-  console.log('Unhandled Rejection !💥Shutdown...');
+  console.log('Unhandled Rejection! 💥 Shutdown...');
   console.log(err.name, err.message);
   server.close(() => {
     process.exit(1);
   });
 });
 
-// SIGTERM Signals
-process.on('SIGTERM',()=>{
-  console.log('👋 SIGTERM RECIVES. Shutting down gracefully'),
-  server.close(()=>{
-    console.log('💥 Process terminated')
-  })
-})
+/* SIGTERM */
+process.on('SIGTERM', () => {
+  console.log('👋 SIGTERM RECEIVED. Shutting down gracefully');
+  server.close(() => {
+    console.log('💥 Process terminated');
+  });
+});
